fd0232f5ede19dd9ca30ecbe1cba05de53fdae0e
mathieu <mathieu.hautebas@gmail.com>
2020-05-31 19:12:19 +0200

    add register_data

--- api/vdm/api.py
+++ api/vdm/api.py
@@ -1,7 +1,12 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return "Welcome to VDM escape game\n"
+    return "Welcome to VDM escape game\n"
+
+@app.route('/register', methods=["POST"])
+def register_data():
+    print(request.json)
+    return jsonify({"message" : "created"}), 201
