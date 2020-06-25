7179eeed9235ed6fffadbaeb197981237e70e636
boris <boris.lepage@gmail.com>
2020-06-26 01:11:46 +0200

    blupblop to Matteyeux

--- vdm_project/dashboard/views.py
+++ vdm_project/dashboard/views.py
@@ -1,9 +1,15 @@
+import requests 
+import json
 from django.shortcuts import render
 
+URL = 'http://127.0.0.1:5000/reservations/'
 
 def vdm_report_page(request):
     return render(request, 'order_report.html', {})
 
 def vdm_list_booking_page(request):
-    bookings_data = Booking.object.all()
-    return render(request, 'list_booking.html', {})
+    # bookings_data = Booking.object.all()
+    bookings = requests.get(URL).text
+    return render(request, 'list_booking.html', {
+    	'bookings': bookings,
+    	})
