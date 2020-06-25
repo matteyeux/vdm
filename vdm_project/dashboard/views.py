258157e74583c2af4cd3a6f3d7898ea6574b892a
Boris <boris.lepage@gmail.com>
2020-06-25 12:19:59 +0200

    [Feature] First view Flexmonster need to connect to mongodb to work in it

--- vdm_project/dashboard/views.py
+++ vdm_project/dashboard/views.py
@@ -1,12 +1,5 @@
-from django.http import JsonResponse
 from django.shortcuts import render
-from dashboard.models import Order
-from django.core import serializers
 
-def dashboard_with_pivot(request):
-    return render(request, 'dashboard_with_pivot.html', {})
 
-def pivot_data(request):
-    dataset = Order.objects.all()
-    data = serializers.serialize('json', dataset)
-    return JsonResponse(data, safe=False)
+def vdm_report_page(request):
+   return render(request, 'order_report.html', {})
