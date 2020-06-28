e614039c90a84725893e516c0c58e98ec98b7808
boris <boris.lepage@gmail.com>
2020-06-29 00:38:52 +0200

    [Feature] add total price in list booking

--- vdm_project/dashboard/templatetags/custom_filters.py
+++ vdm_project/dashboard/templatetags/custom_filters.py
@@ -5,19 +5,21 @@ import datetime
 
 register = template.Library()
 
-@register.filter
-@stringfilter
+# @register.filter
+# @stringfilter
 @register.filter(name='string_to_date')
 def string_to_date(value, arg):
 	date_obj = datetime.datetime.strptime(value, arg)
 	new_date = date_obj.strftime('%d/%m/%Y')
 	return new_date
 
-@register.filter(name='to_float')
-def add_float(value,arg):
-
-    return float(value)
+# @register.filter(name='to_float')
+# def add_price(value):
+# 	total_price = 0.0
+# 	for price in value['prix']
+# 		total_price += float(price)
+# 	return total_price
 
 @register.simple_tag
-def setvar(val=None):
-  return val
+def set_var(val=None):
+	return val
--- vdm_project/dashboard/views.py
+++ vdm_project/dashboard/views.py
@@ -11,6 +11,16 @@ def vdm_list_booking_page(request):
     bookings_data = requests.get(URL).text
     bookings = json.loads(bookings_data)
     print(type(bookings))
+    print(bookings[0]['Reservation'])
+    price = 0.0
+    for booking in bookings:
+    	for spectator in booking['Reservation']:
+    		price += spectator['prix']
+    	price = float("{:.2f}".format(price))	
+    	total_price = {'total_prix': price}
+    	booking.update(total_price)	
+    	print(booking)
+    	price = 0.0
     return render(request, 'list_booking.html', {
     	'bookings': bookings,
     	})
