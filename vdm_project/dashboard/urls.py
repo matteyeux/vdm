b6631db90c4e47f88d60129c60208e01d3b386cd
boris <boris.lepage@gmail.com>
2020-07-01 01:00:58 +0200

    [Feature] Add Ajax to call api every 4 seconds 'WIP'

--- vdm_project/dashboard/urls.py
+++ vdm_project/dashboard/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('report/order', views.vdm_report_page, name='order_report'),
-    path('report/list_booking', views.vdm_list_booking_page, name='list_booking'),
+    path('report/booking_list', views.vdm_booking_list_page, name='booking_list'),
+    path('get/booking_list', views.get_booking_list, name='get_booking_list'),
 ]
