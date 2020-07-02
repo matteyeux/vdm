cccb4d3706d1556855377432c7a0829d0a5aa1ff
boris <boris.lepage@gmail.com>
2020-07-03 00:28:15 +0200

    [Feature] real time are ok, need to create new route to call only 10 first documents

--- vdm_project/dashboard/views.py
+++ vdm_project/dashboard/views.py
@@ -8,7 +8,19 @@ def vdm_report_page(request):
     return render(request, 'order_report.html', {})
 
 def vdm_booking_list_page(request):
-    return render(request, 'booking_list.html', {})
+    bookings_data = requests.get(URL).text
+    bookings = json.loads(bookings_data)
+    price = 0.0
+    for booking in bookings:
+    	for spectator in booking['Reservation']:
+    		price += spectator['prix']
+    	price = float("{:.2f}".format(price))	
+    	total_price = {'total_prix': f'{price:.2f}' + " €"}
+    	booking.update(total_price)
+    	price = 0.0
+    return render(request, 'booking_list.html', {
+    	'bookings': bookings,
+    	})
 
 def get_booking_list(request):
     bookings_data = requests.get(URL).text
@@ -17,7 +29,7 @@ def get_booking_list(request):
     for booking in bookings:
     	for spectator in booking['Reservation']:
     		price += spectator['prix']
-    	price = float("{:.2f}".format(price))	
+    	price = float("'%.2f".format(price))	
     	total_price = {'total_prix': price}
     	booking.update(total_price)
     	price = 0.0
