637d65e8013f85777f2b3d2767787e88aa1779f1
boris <boris.lepage@gmail.com>
2020-07-03 00:28:15 +0200

    [Feature] real time are ok, need to create new route to call only 10 first documents

--- vdm_project/dashboard/templatetags/custom_filters.py
+++ vdm_project/dashboard/templatetags/custom_filters.py
@@ -13,12 +13,11 @@ def string_to_date(value, arg):
 	new_date = date_obj.strftime('%d/%m/%Y')
 	return new_date
 
-# @register.filter(name='to_float')
-# def add_price(value):
-# 	total_price = 0.0
-# 	for price in value['prix']
-# 		total_price += float(price)
-# 	return total_price
+@register.filter(name='number_to_money')
+def string_to_date(value, arg):
+	date_obj = datetime.datetime.strptime(value, arg)
+	new_date = date_obj.strftime('%d/%m/%Y')
+	return new_date
 
 @register.simple_tag
 def set_var(val=None):
