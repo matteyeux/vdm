975619c592b952d641b1e8c8bae64ad0eb35c8de
Boris <boris.lepage@gmail.com>
2020-07-03 15:33:11 +0200

    load all data, need to fix a litle bug of viz

--- vdm_project/dashboard/views.py
+++ vdm_project/dashboard/views.py
@@ -5,34 +5,35 @@ from django.shortcuts import render
 URL = 'http://127.0.0.1:5000/reservations/'
 
 def vdm_report_page(request):
-    return render(request, 'order_report.html', {})
+	return render(request, 'order_report.html', {})
 
 def vdm_booking_list_page(request):
-    bookings_data = requests.get(URL).text
-    bookings = json.loads(bookings_data)
-    price = 0.0
-    for booking in bookings:
-    	for spectator in booking['Reservation']:
-    		price += spectator['prix']
-    	price = float("{:.2f}".format(price))	
-    	total_price = {'total_prix': f'{price:.2f}' + " €"}
-    	booking.update(total_price)
-    	price = 0.0
-    return render(request, 'booking_list.html', {
-    	'bookings': bookings,
-    	})
+	bookings_data = requests.get(URL).text
+	bookings = json.loads(bookings_data)
+	price = 0.0
+	for booking in bookings:
+		for spectator in booking['Reservation']:
+			price += spectator['prix']
+		price = float(f'{price:.2f}')
+		str_price = str(price).replace('.',',')	
+		total_price = {'total_prix': str_price + " €"}
+		booking.update(total_price)
+		price = 0.0
+	return render(request, 'booking_list.html', {
+		'bookings': bookings,
+		})
 
 def get_booking_list(request):
-    bookings_data = requests.get(URL).text
-    bookings = json.loads(bookings_data)
-    price = 0.0
-    for booking in bookings:
-    	for spectator in booking['Reservation']:
-    		price += spectator['prix']
-    	price = float("'%.2f".format(price))	
-    	total_price = {'total_prix': price}
-    	booking.update(total_price)
-    	price = 0.0
-    return render(request, 'get_booking_list.html', {
-    	'bookings': bookings,
-    	})
+	bookings_data = requests.get(URL).text
+	bookings = json.loads(bookings_data)
+	price = 0.0
+	for booking in bookings:
+		for spectator in booking['Reservation']:
+			price += spectator['prix']
+		price = float("'%.2f".format(price))	
+		total_price = {'total_prix': price}
+		booking.update(total_price)
+		price = 0.0
+	return render(request, 'get_booking_list.html', {
+		'bookings': bookings,
+		})
