6b98e26ab5bdad8dd85bd249eba8c7de137fd280
Boris <boris.lepage@gmail.com>
2020-07-03 14:43:22 +0200

    Feature] add some css to beautify front

---
+++ vdm_project/vdm_project/static/pages/jquery.dropify.init.js
@@ -0,0 +1,19 @@
+
+/**
+* Theme: Zircos - Bootstrap 4 Admin Dashboard
+* Author: Coderthemes
+* Email: coderthemes@gmail.com
+* dropify 
+*/
+
+$('.dropify').dropify({
+    messages: {
+        'default': 'Drag and drop a file here or click',
+        'replace': 'Drag and drop or click to replace',
+        'remove': 'Remove',
+        'error': 'Ooops, something wrong appended.'
+    },
+    error: {
+        'fileSize': 'The file size is too big (1M max).'
+    }
+});
