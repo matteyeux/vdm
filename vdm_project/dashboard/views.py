8b9509a18917d1d6b9be64f2c93ced1537a13e90
Boris <boris.lepage@gmail.com>
2020-07-06 11:40:50 +0200

    [Feature] add function numberFormatInt to format integer on front and remove api call in back

--- vdm_project/dashboard/views.py
+++ vdm_project/dashboard/views.py
@@ -8,24 +8,22 @@ def vdm_report_page(request):
 	return render(request, 'order_report.html', {})
 
 def vdm_booking_list_page(request):
-	bookings_kpi = requests.get('http://127.0.0.1:5000/kpiBookingList/').text
-	kpi_list = json.loads(bookings_kpi)
-	total_amount = 0
-	nb_resa = 0
-	nb_spectateur = 0
-	for elem in kpi_list:
-		total_amount += elem['TotalPrice']
-		nb_resa += 1
-		nb_spectateur += elem['NbSpectateur']
+	# bookings_kpi = requests.get('http://127.0.0.1:5000/kpiBookingList/').text
+	# kpi_list = json.loads(bookings_kpi)
+	# total_amount = 0
+	# nb_resa = 0
+	# nb_spectateur = 0
+	# for elem in kpi_list:
+	# 	total_amount += elem['TotalPrice']
+	# 	nb_resa += 1
+	# 	nb_spectateur += elem['NbSpectateur']
 
-	kpi = {
-		'total_amount': total_amount,
-		'nb_resa': nb_resa,
-		'nb_spectateur': nb_spectateur
-	}
-	return render(request, 'booking_list.html', {
-		'kpi': kpi
-		})
+	# kpi = {
+	# 	'total_amount': total_amount,
+	# 	'nb_resa': nb_resa,
+	# 	'nb_spectateur': nb_spectateur
+	# }
+	return render(request, 'booking_list.html', {})
 
 def get_booking_list(request):
 	bookings_data = requests.get(URL).text
