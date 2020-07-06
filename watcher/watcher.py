3f822e87da985fddc9ebdcf6b8f28b9cbad70082
mathieu <mathieu.hautebas@gmail.com>
2020-07-07 00:41:13 +0200

    move watch to a special dir

--- watcher/watcher.py
+++ watcher/watcher.py
@@ -6,25 +6,29 @@ import sys
 import requests
 import json
 from datetime import datetime
+from pathlib import Path
 
 URL = "http://127.0.0.1:5000/reservation"
+BOOKING_DIR = "../generate_booking/jsons/"
 
 
 def send_json(json_file: str):
-    with open("generate_booking/jsons/"+json_file) as js:
+    with open(BOOKING_DIR + json_file) as js:
         json_data = json.load(js)
 
     headers = {'Content-type': 'application/json'}
     query_data = json.dumps(json_data)
     query = requests.post(URL, data=query_data, headers=headers)
-    print(query.status_code)
-    print(query.text)
-    # check error
-    # move to other dir according to error
+    if query.status_code == 201:
+        os.rename(BOOKING_DIR + json_file, "processed/" + json_file)
+    else:
+        os.rename(BOOKING_DIR + json_file, "failed/" + json_file)
 
 
 def main():
-    for json_file in os.listdir("generate_booking/jsons"):
+    Path("processed").mkdir(parents=True, exist_ok=True)
+    Path("failed").mkdir(parents=True, exist_ok=True)
+    for json_file in os.listdir(BOOKING_DIR):
         json_date = json_file.split('_')[0]
         now = datetime.now()
         current_date = now.strftime("%Y%m%d%H%M%S")
