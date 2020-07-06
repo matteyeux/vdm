bc867f1b764872ae7c0b2d91bd9a41a31a1e2d53
mathieu <mathieu.hautebas@gmail.com>
2020-07-07 00:14:53 +0200

    fix json parsing

--- api/vdm/utilities.py
+++ api/vdm/utilities.py
@@ -16,47 +16,49 @@ def handle_prices(data: dict) -> dict:
 		reservation['prix'] = price
 	return data
 
+
 def handle_themes(data: dict) -> dict:
-	"""Set themes according to game."""
-	for theme in data['Game']:
-		if theme['Nom'] == "Impot sur le revenu":
-			first_theme = "Braquage"
-			second_theme = "Stress"
-		elif theme['Nom'] == "Greve de la SNCF":
-			first_theme = "Rapidité"
-			second_theme = "Mythologique"
-		elif theme['Nom'] == "Interminable attente chez le medecin":
-			first_theme = "Stratégie"
-			second_theme = "Psychologie"
-		elif theme['Nom'] == "Soutenance finale":
-			first_theme = "Stress"
-			second_theme = "Rapidité"
-		elif theme['Nom'] == "Mon compte en banque en fin du mois":
-			first_theme = "Mythologique"
-			second_theme = "Braquage"
-		elif theme['Nom'] == "Mariage sans alcool":
-			first_theme = "Santé"
-			second_theme = "Amour"
-		elif theme['Nom'] == "Diner de famille insoutenable":
-			first_theme = "Psychologique"
-			second_theme = "Stratégie"
-		elif theme['Nom'] == "Plus de PQ dans les toilettes":
-			first_theme = "Horreur"
-			second_theme = "Santé"
-		elif theme['Nom'] == "En plein dans la Friendzone":
-			first_theme = "Amour"
-			second_theme = "Horreur"
-		else:
-			first_theme = None
-			second_theme = None
-		theme['theme_pricipal'] = first_theme
-		theme['theme_secondaire'] = second_theme
-	return data
+    """Set themes according to game."""
+    if data['Game']['Nom'] == "Impot sur le revenu":
+        first_theme = "Braquage"
+        second_theme = "Stress"
+    elif data['Game']['Nom'] == "Greve de la SNCF":
+        first_theme = "Rapidité"
+        second_theme = "Mythologique"
+    elif data['Game']['Nom'] == "Interminable attente chez le medecin":
+        first_theme = "Stratégie"
+        second_theme = "Psychologie"
+    elif data['Game']['Nom'] == "Soutenance finale":
+        first_theme = "Stress"
+        second_theme = "Rapidité"
+    elif data['Game']['Nom'] == "Mon compte en banque en fin du mois":
+        first_theme = "Mythologique"
+        second_theme = "Braquage"
+    elif data['Game']['Nom'] == "Mariage sans alcool":
+        first_theme = "Santé"
+        second_theme = "Amour"
+    elif data['Game']['Nom'] == "Diner de famille insoutenable":
+        first_theme = "Psychologique"
+        second_theme = "Stratégie"
+    elif data['Game']['Nom'] == "Plus de PQ dans les toilettes":
+        first_theme = "Horreur"
+        second_theme = "Santé"
+    elif data['Game']['Nom'] == "En plein dans la Friendzone":
+        first_theme = "Amour"
+        second_theme = "Horreur"
+    else:
+        first_theme = None
+        second_theme = None
+    data['Game']['theme_pricipal'] = first_theme
+    data['Game']['theme_secondaire'] = second_theme
+    return data
+
 
 def handle_datetime(data: dict) -> dict:
 	"""Set fields CreatedAt."""
 	data['CreatedAt'] = datetime.datetime.now()
 	return data
+
 
 def handle_utilities(data: dict) -> dict:
 	"""Call each function to transform the data dict."""
