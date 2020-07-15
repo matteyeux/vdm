97a535f62095be32a6fa3cece066481c338797cd
humeau_y <humeau_y@etna-alternance.net>
2020-07-15 22:45:30 +0200

    Add some others stats

--- api/vdm/api.py
+++ api/vdm/api.py
@@ -14,8 +14,13 @@ from themes import (ns_caThemeDays, ns_cadThemeFirst,
                     ns_cadThemeSecond, ns_ptRoomThemesF, ns_ptRoomThemesS)
 from extract import ns_extractKPIDaily, ns_extractKPIHisto 
 from customer import (ns_customer_civilities, ns_customer_civilities_daily,
-                    ns_spectators_civilities, ns_spectators_civilities_daily,
-                    ns_customer_version, ns_customer_version_daily)
+                    ns_spectator_civilities, ns_spectator_civilities_daily,
+                    ns_customer_version, ns_customer_version_daily,
+                    ns_spectator_version, ns_spectator_version_daily,
+                    #ns_customerBookingsHours, #ns_spectatorsBookingsHours                
+                    ns_customer_game_hours, #ns_spectatorGamehours,
+                    #ns_customerSplitAge, #ns_spectatorSplitAge                    
+                    )
 
 app = Flask(__name__)
 api = Api(app=app, version='0.1.0', title='VDM Api',
@@ -57,11 +62,22 @@ api.add_namespace(ns_extractKPIHisto)
 
 api.add_namespace(ns_customer_civilities)
 api.add_namespace(ns_customer_civilities_daily)
-api.add_namespace(ns_spectators_civilities)
-api.add_namespace(ns_spectators_civilities_daily)
+#api.add_namespace(ns_spectator_civilities)
+#api.add_namespace(ns_spectator_civilities_daily)
 
 api.add_namespace(ns_customer_version)
 api.add_namespace(ns_customer_version_daily)
+api.add_namespace(ns_spectator_version)
+api.add_namespace(ns_spectator_version_daily)
+
+#ns_customerBookingsHours
+#ns_spectatorsBookingsHours
+
+api.add_namespace(ns_customer_game_hours)
+#ns_spectatorGamehours
+
+#ns_customerSplitAge
+#ns_spectatorSplitAge
 
 @ns_root.route('/')
 class default_root(Resource):
--- api/vdm/customer.py
+++ api/vdm/customer.py
@@ -9,22 +9,22 @@ import config
 
 ns_customer_civilities = Namespace('customer/civilities', description="statistics stuff")
 ns_customer_civilities_daily = Namespace('customer/civilities/daily', description="statistics stuff")
-ns_spectators_civilities = Namespace('spectator/civilities', description="Histo split man / wooman")
-ns_spectators_civilities_daily = Namespace('spectator/civilities/daily', description="Daily split man / wooman")
+ns_spectator_civilities = Namespace('spectator/civilities', description="Histo split man / wooman")
+ns_spectator_civilities_daily = Namespace('spectator/civilities/daily', description="Daily split man / wooman")
 
 ns_customer_version = Namespace('version', description="Histo split VR / Not VR")
 ns_customer_version_daily = Namespace('version/daily', description="Daily split VR / Not VR")
-ns_spectators_version = Namespace('spectators/version', description="Histo split VR / Not VR")
-ns_spectators_version_daily = Namespace('spectators/version/daily', description="Daily split VR / Not VR")
+ns_spectator_version = Namespace('spectator/version', description="Histo split VR / Not VR")
+ns_spectator_version_daily = Namespace('spectator/version/daily', description="Daily split VR / Not VR")
 
-ns_customerBookingsHours = Namespace('customerHours', description="dispatch customer by hours")
-ns_spectatorsBookingsHours = Namespace('spectatorsHours', description="dispatch Spectators by hours")
+ns_customerBookingsHours = Namespace('customer/hours', description="dispatch customer by hours")
+ns_spectatorsBookingsHours = Namespace('spectators/hours', description="dispatch Spectators by hours")
 
-ns_customerGamehours = Namespace('customerGameHours', description="dispatch customer by Games hours")
-ns_spectatorsGamehours = Namespace('spectatorsGameHours', description="dispatch Spectators by Games hours")
+ns_customer_game_hours = Namespace('customer/game/hours', description="dispatch customer by Games hours")
+ns_spectatorGamehours = Namespace('spectator/game/hours', description="dispatch Spectators by Games hours")
 
 ns_customerSplitAge = Namespace('customerhours', description="dispatch customer by hours")
-ns_spectatorsSplitAge = Namespace('spectatorshours', description="dispatch Spectators by hours")
+ns_spectatorSplitAge = Namespace('spectator/hours', description="dispatch Spectators by hours")
 
 
 @ns_customer_civilities.route("/")
@@ -94,7 +94,7 @@ class CustomerCivilityDay(Resource):
         return response
 
 
-@ns_spectators_civilities.route("/")
+@ns_spectator_civilities.route("/")
 class SpectatorCivility(Resource):
     def get(self):  
         """Get reservation by ID."""
@@ -127,8 +127,8 @@ class SpectatorCivility(Resource):
         return response
 
 
-@ns_spectators_civilities_daily.route("")
-class SpectatorCivilityDay(Resource):
+@ns_spectator_civilities_daily.route("")
+class SpectatorCivilityDaily(Resource):
     def get(self):  
         """Get reservation by ID."""
         vdm_database = config.setup_mongo()
@@ -162,7 +162,7 @@ class SpectatorCivilityDay(Resource):
 
 
 @ns_customer_version.route("/")
-class CustomerCivility(Resource):
+class CustomerVersion(Resource):
     def get(self):  
         """Get reservation by ID."""
         vdm_database = config.setup_mongo()
@@ -195,7 +195,7 @@ class CustomerCivility(Resource):
 
 
 @ns_customer_version_daily.route("")
-class CustomerCivilityDay(Resource):
+class CustomerVersionDaily(Resource):
     def get(self):  
         """Get reservation by ID."""
         vdm_database = config.setup_mongo()
@@ -226,4 +226,108 @@ class CustomerCivilityDay(Resource):
         response.headers.add('Access-Control-Allow-Origin', '*')
         response.status_code = 200
         return response
+
+
+@ns_spectator_version.route("/")
+class SpectatorVersion(Resource):
+    def get(self):  
+        """Get reservation by ID."""
+        vdm_database = config.setup_mongo()
+        cursor = vdm_database.booking.aggregate([
+            {
+                "$project":
+                {
+                    "Game.VR": 1,
+                    "Nb": {"$size": "$Reservation"} 
+                }
+            },
+            {
+                "$group":
+                {
+                    "_id": "$Game.VR",
+                    "total": {"$sum": "$Nb"}
+                }
+            }
+        ])
+
+        data = []
+        for reservation in cursor:
+            res_str =json.loads(dumps(reservation))
+            data.append(res_str)
+
+        response = jsonify(data)
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        response.status_code = 200
+        return response
+
+
+@ns_spectator_version_daily.route("")
+class SpectatorVersionDaily(Resource):
+    def get(self):  
+        """Get reservation by ID."""
+        vdm_database = config.setup_mongo()
+        cursor = vdm_database.booking.aggregate([
+            {
+                "$project":
+                {
+                    "date": {"$dateToString": {"format": "%d/%m/%Y", "date": "$CreatedAt"}},
+                    "Game.VR": 1,
+                    "Nb": {"$size": "$Reservation"} 
+                }
+            },
+            {
+                "$group":
+                {
+                    "_id": {"date": "$date", "Civilite": "$Game.VR"},
+                    "total": {"$sum": "$Nb"}
+                }
+            }
+        ])
+
+        data = []
+        for reservation in cursor:
+            res_str =json.loads(dumps(reservation))
+            data.append(res_str)
+
+        response = jsonify(data)
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        response.status_code = 200
+        return response
+
+
+@ns_customer_game_hours.route("/")
+class CustomerBookingHours(Resource):
+    def get(self):  
+        """Get reservation by ID."""
+        vdm_database = config.setup_mongo()
+        cursor = vdm_database.booking.aggregate([
+            {
+                "$project":
+                {
+                    "Game.Horaire": 1,
+                    "Nb": {"$sum": 1} 
+                }
+            },
+            {
+                "$group":
+                {
+                    "_id": "$Game.Horaire",
+                    "total": {"$sum": "$Nb"}
+                }
+            }
+        ])
+
+        data = []
+        for reservation in cursor:
+            res_str =json.loads(dumps(reservation))
+            data.append(res_str)
+
+        response = jsonify(data)
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        response.status_code = 200
+        return response
+
+
+
+
 
