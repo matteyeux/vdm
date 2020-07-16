90a22be1610be9fb371b66816395f77bc9acd457
mathieu <mathieu.hautebas@gmail.com>
2020-07-16 20:06:18 +0200

    add log

--- api/vdm/main.py
+++ api/vdm/main.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 import sys
+import logging
 from vdm import api
 
 """Main module for vdm api."""
 
 def main():
+    logging.basicConfig(filename='vdm_error.log',level=logging.DEBUG)
     if len(sys.argv) == 2 and sys.argv[1] == "-s":
         context = ('vdm_certs/fullchain.pem', 'vdm_certs/privkey.pem')
         api.app.run(host='0.0.0.0', ssl_context=context)
